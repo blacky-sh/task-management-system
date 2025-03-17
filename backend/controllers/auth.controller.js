bb1afd64ebac9dc08d1f9262b8b3f217b6daf6c2
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 10:31:25 +0300

    feat: Implement basic user signup with password hashing

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,31 @@
-export const signup = async () => {};
+import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 export const verifyEmail = async () => {};
 
