305c21832491cfbfd072b4cee93bbdd68c394d1e
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 20:07:37 +0300

    chore: Initialize Mailtrap client and define sender details

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,10 @@
+import { MailtrapClient } from "mailtrap";
+
+export const mailtrapClient = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Task Management System",
+};
