82024e6eb502375da85a984e430b618a3dc5c231
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 11:37:50 +0300

    feat: Secure /check-auth route with JWT authentication

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
+import { signup, checkAuth } from "../controllers/auth.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
+
+router.post("/signup", signup);
 
 export default router;
