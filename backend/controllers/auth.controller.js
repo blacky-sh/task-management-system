0d8e78b0600366c744db3cba968ad74aaa95535c
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 10:58:29 +0300

    feat: Implement email verification sending in signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
+import { sendVerificationEmail } from "../mailtrap/emails.js";
 
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -40,6 +41,8 @@ export const signup = async (req, res) => {
 
     // jwt
     generateTokenAndSetCookie(res, user._id);
+
+    await sendVerificationEmail(user.email, verificationToken);
 
     res.status(201).json({
       success: true,
