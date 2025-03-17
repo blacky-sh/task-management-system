464b5566989a33bb50bb3b85fd5b206908225fc8
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 19:55:07 +0300

    feat: Implement forgot password endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signup, checkAuth, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  checkAuth,
+  logout,
+  verifyEmail,
+  forgotPassword,
+} from "../controllers/auth.controller.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
@@ -8,5 +14,8 @@ router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 router.post("/logout", logout);
+
+router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
 
 export default router;
