18b85f5abad09b05bea08edad580505b8ba8b9ea
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 20:00:56 +0300

    feat: Implement reset password endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -6,6 +6,7 @@ import {
   logout,
   verifyEmail,
   forgotPassword,
+  resetPassword,
 } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
@@ -19,5 +20,6 @@ router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
