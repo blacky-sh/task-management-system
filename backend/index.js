02fb9450bc7d9fe65bc0edc5bb2ecbb9fed0527b
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 20:04:41 +0300

    chore: Add middleware for JSON parsing, cookie parsing, and auth routing

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ import { connectDB } from "./database/connectDB.js";
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json()); // middleware that allows us to parse incoming requests with json data: req.body
+app.use(cookieParser()); // middleware that allows us to parse incoming requests with cookies: req.cookies
+
+app.use("/api/auth", authRouter);
+
 app.listen(port, () => {
   connectDB();
   console.log(`Server is running on port ${port}`);
