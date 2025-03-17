dc57856426aedf790c130c3954ade2e6643dface
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 10:26:13 +0300

    feat: Add empty auth controller functions

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = async () => {};
+
+export const verifyEmail = async () => {};
+
+export const login = async () => {};
+
+export const logout = async () => {};
+
+export const forgotPassword = async () => {};
+
+export const resetPassword = async () => {};
+
+export const checkAuth = async () => {};
