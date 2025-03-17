863a0bbb2b7ca503bdcc8e6e3e4719bc65958e17
Shehabedin Ahmed Ali <shahabahmed20001@gmail.com>
2025-03-17 11:35:59 +0300

    feat: Implement checkAuth endpoint for verifying authentication

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -69,4 +69,18 @@ export const forgotPassword = async () => {};
 
 export const resetPassword = async () => {};
 
-export const checkAuth = async () => {};
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in checkAuth ", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
